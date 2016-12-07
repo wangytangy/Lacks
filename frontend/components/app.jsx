d5286b43a761c246cdf6c0f14dd94014fbd26925
Bruce <yue.bruce.wang@gmail.com>
2016-12-06 20:59:53 -0500

    Change email require to null true

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
 
 function App({ children }) {
+
   return(
     <div>
-      <h1>Lacks</h1>
       <GreetingContainer />
       {children}
     </div>
