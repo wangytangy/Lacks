3080e46616543b47ce8c401cdc1b492c1e8efb6e
Yue Wang <yue.bruce.wang@gmail.com>
2016-12-12 18:05:39 -0500

    Refactor join channel action creator

--- frontend/reducers/channels_reducer.js
+++ frontend/reducers/channels_reducer.js
@@ -1,16 +1,21 @@
-import { receiveChannels, deleteChannel } from '../actions/channels_actions';
-import { RECEIVE_ALL_CHANNELS, DELETE_CHANNEL } from '../actions/channels_actions';
+import { receiveChannels, deleteChannel, joinChannel } from '../actions/channels_actions';
+import { RECEIVE_ALL_CHANNELS, DELETE_CHANNEL, RECEIVE_JOIN_CHANNEL } from '../actions/channels_actions';
 
 
 
 function ChannelsReducer(state = {}, action) {
   Object.freeze(state);
+  let copyState;
   switch(action.type) {
     case RECEIVE_ALL_CHANNELS:
       return action.channels;
     case DELETE_CHANNEL:
-      let copyState = Object.assign({}, state);
+      copyState = Object.assign({}, state);
       delete copyState[action.channel.id];
+      return copyState;
+    case RECEIVE_JOIN_CHANNEL:
+      copyState = Object.assign({}, state);
+      copyState[action.joinedChannel.id] = action.joinedChannel;
       return copyState;
     default:
       return state;
