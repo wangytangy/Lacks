48ee5593fb141619afb414175015418e6f3efeb4
Yue Wang <yue.bruce.wang@gmail.com>
2016-12-14 19:23:15 -0500

    Update Direct Message add members

--- frontend/components/direct_messages/create_direct_messages.jsx
+++ frontend/components/direct_messages/create_direct_messages.jsx
@@ -6,6 +6,8 @@ class CreateDirectMessages extends React.Component {
     this.handleInputChange = this.handleInputChange.bind(this);
     this.filterUsersbyInput = this.filterUsersbyInput.bind(this);
     this.allUsersList = this.allUsersList.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.renderSelectedUsers = this.renderSelectedUsers.bind(this);
     this.state = {
       friends: [],
       searchInput: ""
@@ -16,15 +18,30 @@ class CreateDirectMessages extends React.Component {
     this.props.fetchAllUsers();
   }
 
+  handleClick(e) {
+    let updatedFriends = this.state.friends;
+    let selectedUser = e.currentTarget.type;
+    if (this.state.friends.includes(selectedUser)) {
+      let deleteIdx = updatedFriends.indexOf(selectedUser);
+      updatedFriends.splice(deleteIdx, 1);
+      this.setState({friends: updatedFriends});
+    } else {
+      updatedFriends.push(selectedUser);
+      this.setState({friends: updatedFriends});
+    }
+  }
+
+
   allUsersList(searchedUsers) {
     let usersElements = [];
     searchedUsers.forEach((user, i) => {
       let liElement = (
         <li
           key={i}
+          onClick={this.handleClick}
+          type={user.username}
         >
           <div className="search-result group">
-
             <div className="search-result-topline group">
               <h3>{user.username}</h3>
             </div>
@@ -44,7 +61,6 @@ class CreateDirectMessages extends React.Component {
 
   filterUsersbyInput() {
     let filteredUsers = [];
-
     filteredUsers = this.props.users.filter((user) => {
       if (user.username.indexOf(this.state.searchInput) !== -1) {
         return true;
@@ -53,29 +69,56 @@ class CreateDirectMessages extends React.Component {
     return filteredUsers;
   }
 
+  renderSelectedUsers() {
+    let selectedUsers = [];
+    this.state.friends.forEach((friend, i) => {
+      let liElement = (
+        <li
+          key={i}
+          className="dm-selected-item"
+          type={friend}
+          onClick={this.handleClick}
+        >
+          <div className="dm-details-container">
+            <p>{friend}</p>
+            <i className="material-icons clear">
+              clear
+            </i>
+          </div>
+        </li>
+      );
+      selectedUsers.push(liElement);
+    });
+    return selectedUsers;
+  }
 
   render() {
     let usersElements = this.allUsersList(this.filterUsersbyInput());
+    let selectedUsers = this.renderSelectedUsers();
+    console.log(selectedUsers);
 
     return(
-      <div className="browse-channels">
+      <div className="browse-dm">
         <h1>Direct Messages</h1>
-
-        <div className="browse-channel-searchbar">
+        <div className="dm-selected-container">
+          {selectedUsers}
+        </div>
+        <div className="browse-dm-searchbar">
           <i className="material-icons search">search</i>
           <input
             type="text"
-            className="browse-search"
+            className="dm-search"
             placeholder="Find or start a conversation"
             onChange={this.handleInputChange}
             >
           </input>
         </div>
 
-        <h2>Recent conversations</h2>
+        <h2>Find or start a conversation</h2>
         <ul>
           {usersElements}
         </ul>
+
       </div>
     );
   }
