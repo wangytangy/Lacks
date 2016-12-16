e9486b6dbebdde4d5692ba0ed494e6cf870273e0
Yue Wang <yue.bruce.wang@gmail.com>
2016-12-16 10:48:27 -0500

    Finalize seeds

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,19 +2,6 @@ class Api::UsersController < ApplicationController
 
   helper_method :current_user, :logged_in?
 
-  PROFILE_PICS = [
-    "http://pix.iemoji.com/images/emoji/apple/ios-9/256/grinning-face.png",
-    "https://cdn-assets.insomniac.com/emoji_sexface.png",
-    "http://pix.iemoji.com/images/emoji/apple/ios-9/256/flushed-face.png",
-    "http://cdn.shopify.com/s/files/1/0185/5092/products/persons-0006.png?v=1369544093",
-    "http://pix.iemoji.com/images/emoji/apple/ios-9/256/dog-face.png",
-    "http://pix.iemoji.com/images/emoji/apple/ios-9/256/pouting-face.png",
-    "http://cdn.playbuzz.com/cdn/ef15cfd0-01f0-466c-97b8-56adae1450bd/409312fb-e43e-42c9-a69c-7cdda7a76281.png",
-    "http://pix.iemoji.com/images/emoji/apple/ios-9/256/sleeping-face.png",
-    "http://pix.iemoji.com/images/emoji/apple/ios-9/256/smiling-face-with-smiling-eyes.png",
-    "http://pix.iemoji.com/images/emoji/apple/ios-9/256/thumbs-up-sign.png"
-  ]
-
   def index
     @users = User.all.where.not(["id = ?", current_user.id])
     render :index
@@ -22,8 +9,8 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.profile_pic_url = PROFILE_PICS[rand(0...10)]
-    
+    @user.profile_pic_url = Faker::Avatar.image(Faker::Name.first_name, "50x50")
+
     if @user.save
       login(@user)
       if current_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,6 @@ guest = User.create!(username: "guest", email: "guest@gmail.com", password: "gue
 
 all_users = []
 all_users << guest
-all_users << admin
 20.times do
   all_users << User.create!(
     username: Faker::Internet.user_name,
@@ -27,21 +26,27 @@ end
 
 
 # CHANNELS
-Channel.destroy_all
 
-general = Channel.create!(title: "general", description: "This channel is for general discussion", user_id: admin.id)
-channel1 = Channel.create!(title: Faker::Address.country, description: Faker::Hipster.sentence, user_id: guest.id)
-channel2 = Channel.create!(title: Faker::Address.country, description: Faker::Hipster.sentence, user_id: guest.id)
-channel3 = Channel.create!(title: Faker::Address.country, description: Faker::Hipster.sentence, user_id: guest.id)
-channel4 = Channel.create!(title: Faker::Address.country, description: Faker::Hipster.sentence, user_id: guest.id)
-channel5 = Channel.create!(title: Faker::Address.country, description: Faker::Hipster.sentence, user_id: guest.id)
-channel6 = Channel.create!(title: Faker::Address.country, description: Faker::Hipster.sentence, user_id: guest.id)
-channel7 = Channel.create!(title: Faker::Address.country, description: Faker::Hipster.sentence, user_id: guest.id)
-channel8 = Channel.create!(title: Faker::Address.country, description: Faker::Hipster.sentence, user_id: guest.id)
-channel9 = Channel.create!(title: Faker::Address.country, description: Faker::Hipster.sentence, user_id: guest.id)
-channel10 = Channel.create!(title: Faker::Address.country, description: Faker::Hipster.sentence, user_id: guest.id)
+Channel.destroy_all
+all_channels = [
+  general = Channel.create!(title: "general", description: "This channel is for general discussion", user_id: admin.id),
+  channel1 = Channel.create!(title: "2016-10-17 NYC", description: "the best cohort", user_id: admin.id),
+  channel2 = Channel.create!(title: "Snack", description: "chat group about snacks", user_id: admin.id),
+  channel3 = Channel.create!(title: "MapMyTrot", description: "we're not horsing around", user_id: admin.id),
+  channel4 = Channel.create!(title: "Slack", description: "A messaging app for teams", user_id: admin.id),
+  channel5 = Channel.create!(title: "goodBooks", description: "check out these books", user_id: admin.id),
+  channel6 = Channel.create!(title: "Chat Academy", description: "serious coders only", user_id: admin.id),
+  channel7 = Channel.create!(title: "2016-09-12 SF", description: "doing stuff in SF", user_id: admin.id),
+  channel8 = Channel.create!(title: "Spadebook", description: "a chat group for shovel enthusiasts", user_id: admin.id),
+  channel9 = Channel.create!(title: "FrugalSheets", description: "most reputable app in the world", user_id: admin.id)
+]
 
 ChannelMembership.destroy_all
+
+# join "admin" to all the channels created by her/him
+all_channels.each do |channel|
+  ChannelMembership.create!(member_id: admin.id, channel_id: channel.id)
+end
 
 # join all users to "general" channel
 all_users.each do |user|
@@ -49,23 +54,71 @@ all_users.each do |user|
 end
 
 
+#grab some users and join them to some channels
+all_users[0..2].each do |user|
+  ChannelMembership.create!(member_id: user.id, channel_id: channel1.id)
+end
 
+all_users[3..5].each do |user|
+  ChannelMembership.create!(member_id: user.id, channel_id: channel3.id)
+end
 
+all_users[6..8].each do |user|
+  ChannelMembership.create!(member_id: user.id, channel_id: channel5.id)
+end
 
+all_users[9..11].each do |user|
+  ChannelMembership.create!(member_id: user.id, channel_id: channel7.id)
+end
 
-
-#grab samples of users and join them to random channels
 
 
 #grab samples of users and join them to random DM's
+dm1_title = all_users[19].username + "," + all_users[18].username + "," + all_users[17].username
+dm1 = Channel.create!(
+  title: dm1_title,
+  description: "",
+  direct_message_status: true,
+  user_id: all_users[16].id
+)
+ChannelMembership.create!(member_id: all_users[19].id, channel_id: dm1.id)
+ChannelMembership.create!(member_id: all_users[18].id, channel_id: dm1.id)
+ChannelMembership.create!(member_id: all_users[17].id, channel_id: dm1.id)
+ChannelMembership.create!(member_id: all_users[16].id, channel_id: dm1.id)
+
+dm2_title = all_users[15].username + "," + all_users[14].username + "," + all_users[13].username
+dm2 = Channel.create!(
+  title: dm2_title,
+  description: "",
+  direct_message_status: true,
+  user_id: all_users[12].id
+)
+ChannelMembership.create!(member_id: all_users[15].id, channel_id: dm2.id)
+ChannelMembership.create!(member_id: all_users[14].id, channel_id: dm2.id)
+ChannelMembership.create!(member_id: all_users[13].id, channel_id: dm2.id)
+ChannelMembership.create!(member_id: all_users[12].id, channel_id: dm2.id)
 
 
-# ChannelMembership.create!(channel_id: channel0.id, member_id: guest.id)
-# ChannelMembership.create!(channel_id: channel0.id, member_id: user1.id)
-# ChannelMembership.create!(channel_id: channel0.id, member_id: user2.id)
-# ChannelMembership.create!(channel_id: channel0.id, member_id: user3.id)
-#
-#
-# ChannelMembership.create!(channel_id: channel6.id, member_id: user1.id)
-# ChannelMembership.create!(channel_id: channel6.id, member_id: user2.id)
-# ChannelMembership.create!(channel_id: channel7.id, member_id: user3.id)
+dm3_title = all_users[11].username + "," + all_users[10].username + "," + all_users[9].username
+dm3 = Channel.create!(
+  title: dm3_title,
+  description: "",
+  direct_message_status: true,
+  user_id: all_users[8].id
+)
+ChannelMembership.create!(member_id: all_users[11].id, channel_id: dm3.id)
+ChannelMembership.create!(member_id: all_users[10].id, channel_id: dm3.id)
+ChannelMembership.create!(member_id: all_users[9].id, channel_id: dm3.id)
+ChannelMembership.create!(member_id: all_users[8].id, channel_id: dm3.id)
+
+dm4_title = all_users[7].username + "," + all_users[6].username + "," + all_users[5].username
+dm4 = Channel.create!(
+  title: dm4_title,
+  description: "",
+  direct_message_status: true,
+  user_id: all_users[4].id
+)
+ChannelMembership.create!(member_id: all_users[7].id, channel_id: dm3.id)
+ChannelMembership.create!(member_id: all_users[6].id, channel_id: dm3.id)
+ChannelMembership.create!(member_id: all_users[5].id, channel_id: dm3.id)
+ChannelMembership.create!(member_id: all_users[4].id, channel_id: dm3.id)
